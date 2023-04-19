f0b32990eeb081adfc9d7a48a8153930eb9ba8c3
DESKTOP-G800GM0\uriyu <uriyuri00@gmail.com>
2023-04-18 20:29:10 -0700

    final

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,63 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookId: String!, $description: String!, $title: String!) {
+    saveBook(bookId: $bookId, description: $description, title: $title) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        description
+        title
+        link
+        authors
+        image
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        description
+        title
+        link
+        authors
+        image
+      }
+    }
+  }
+`;
--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -18,6 +18,9 @@ module.exports = {
   },
   // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
   async createUser({ body }, res) {
+    console.log("username,email,password?")
+    console.log(body)
+    
     const user = await User.create(body);
 
     if (!user) {
